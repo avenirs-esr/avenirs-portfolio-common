a1e22946179c7bac0a2cea57123996d774b1ceed
Mouhamed Diallo <mouhamed.diallo409@gmail.com>
2025-11-20 16:32:00 +0100

    feat(self-knowledge): add code error for self knowledge rating

--- src/main/java/fr/avenirsesr/portfolio/common/error/domain/model/enums/EErrorCode.java
+++ src/main/java/fr/avenirsesr/portfolio/common/error/domain/model/enums/EErrorCode.java
@@ -29,6 +29,7 @@ public enum EErrorCode {
   CONFIGURATION_ERROR("Configuration error"),
   TITLE_TOO_LONG("The length of the title is too long"),
   DESCRIPTION_TOO_LONG("The length of the description is too long"),
+  RATING_OUT_OF_BOUNCE("Rating is out of bounce"),
   ASSOCIATION_NOT_FOUND("Association not found"),
   SELF_KNOWLEDGE_ELEMENT_NOT_FOUND("Self knowledge element not found"),
   SELF_KNOWLEDGE_CATEGORY_LIST_EMPTY("Self knowledge category list is empty"),
