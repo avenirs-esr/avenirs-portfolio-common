a2a5ed2f3d8844effcd76922326eb6a00401fc08
Nathan <nathan.pignon79@gmail.com>
2025-12-11 17:01:11 +0100

    feat(829): add period utils with period status
    # Conflicts:
    #	src/main/java/fr/avenirsesr/portfolio/common/temporal/domain/model/enums/EPeriodStatus.java

--- src/main/java/fr/avenirsesr/portfolio/common/temporal/domain/model/enums/EPeriodStatus.java
+++ src/main/java/fr/avenirsesr/portfolio/common/temporal/domain/model/enums/EPeriodStatus.java
@@ -1,7 +1,28 @@
 package fr.avenirsesr.portfolio.common.temporal.domain.model.enums;
 
+import java.time.LocalDate;
+
+import static fr.avenirsesr.portfolio.common.validation.domain.utils.FieldValidationUtils.requireNotNull;
+import static fr.avenirsesr.portfolio.common.validation.domain.utils.FieldValidationUtils.validateDateOrder;
+
 public enum EPeriodStatus {
     BEFORE,
     DURING,
-    AFTER
+    AFTER;
+
+    public static EPeriodStatus of(LocalDate startDate, LocalDate endDate) {
+        requireNotNull("startDate", startDate);
+        validateDateOrder(startDate, endDate);
+        LocalDate today = LocalDate.now();
+
+        if (today.isBefore(startDate)) {
+            return BEFORE;
+        }
+
+        if (endDate == null || !today.isAfter(endDate)) {
+            return DURING;
+        }
+
+        return AFTER;
+    }
 }
