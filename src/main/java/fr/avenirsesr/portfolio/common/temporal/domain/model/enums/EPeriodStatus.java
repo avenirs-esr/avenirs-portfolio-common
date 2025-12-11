5a4c156b389cc9da3da4693985d2b0aa050c2260
Nathan <nathan.pignon79@gmail.com>
2025-12-11 17:01:11 +0100

    feat(829): add period utils with period status

---
+++ src/main/java/fr/avenirsesr/portfolio/common/temporal/domain/model/enums/EPeriodStatus.java
@@ -0,0 +1,7 @@
+package fr.avenirsesr.portfolio.common.temporal.domain.model.enums;
+
+public enum EPeriodStatus {
+    BEFORE,
+    DURING,
+    AFTER
+}
---
+++ src/main/java/fr/avenirsesr/portfolio/common/validation/domain/constraints/FieldMaxLengths.java
@@ -0,0 +1,13 @@
+package fr.avenirsesr.portfolio.common.validation.domain.constraints;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class FieldMaxLengths {
+
+    public static final int TITLE_LENGTH = 80;
+    public static final int ORGANIZATION_LENGTH = 50;
+    public static final int DESCRIPTION_LENGTH = 400;
+    public static final int RESULT_LENGTH = 50;
+    public static final int SOURCE_OF_INFORMATION_LENGTH = 200;
+}
