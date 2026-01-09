0f134c49b1e74cf660762a719fdbd2ffd8927323
Nathan <nathan.pignon79@gmail.com>
2026-01-09 15:18:42 +0100

    feat(856): create common limits class

---
+++ src/main/java/fr/avenirsesr/portfolio/common/validation/domain/constraints/CommonLimits.java
@@ -0,0 +1,9 @@
+package fr.avenirsesr.portfolio.common.validation.domain.constraints;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class CommonLimits {
+  public static final int MAX_IMPORTED_SKILLS = 6;
+  public static final int MAX_TRACES_OVERVIEW = 3;
+}
