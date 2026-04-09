e400d65f494c035b65d54bb21f775bea06b5dac6
iamgreendev <56599296+BanelhaqB@users.noreply.github.com>
2026-04-09 15:04:11 +0200

    fix(trace): fix trace filter specification builder

--- src/main/java/fr/avenirsesr/portfolio/common/data/infrastructure/adapter/specification/FilterSpecificationBuilder.java
+++ src/main/java/fr/avenirsesr/portfolio/common/data/infrastructure/adapter/specification/FilterSpecificationBuilder.java
@@ -1,20 +1,22 @@
 package fr.avenirsesr.portfolio.common.data.infrastructure.adapter.specification;
 
 import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.data.jpa.domain.Specification;
 
 public abstract class FilterSpecificationBuilder<T, K extends Enum<K>> {
-  public Specification<T> build(Map<K, Object> filters) {
-    Specification<T> spec = Specification.where((Specification<T>) null);
+  public Optional<Specification<T>> build(Map<K, Object> filters) {
+    Specification<T> spec = null;
 
     for (var entry : filters.entrySet()) {
       Specification<T> s = getSpecification(entry.getKey(), entry.getValue());
       if (s != null) {
-        spec = spec.and(s);
+        spec = spec == null ? Specification.where(s) : spec.and(s);
       }
     }
 
-    return spec;
+    return Optional.ofNullable(spec);
   }
 
   protected abstract Specification<T> getSpecification(K key, Object value);
