ee3a18eb10211f9e2fcbaa9e875d796bef583bf3
Nathan <nathan.pignon79@gmail.com>
2026-05-25 11:01:19 +0200

    feat(1679): create user status enum

---
+++ src/main/java/fr/avenirsesr/portfolio/common/user/application/adapter/dto/ExternalUserDTO.java
@@ -0,0 +1,19 @@
+package fr.avenirsesr.portfolio.common.user.application.adapter.dto;
+
+import fr.avenirsesr.portfolio.common.data.domain.model.enums.EUserCategory;
+import fr.avenirsesr.portfolio.common.user.domain.model.enums.EUserStatus;
+
+public record ExternalUserDTO(
+        String eppn,
+        String firstName,
+        String lastName,
+        String email,
+        EUserCategory category,
+        String externalId,
+        String source,
+        EUserStatus status) {
+
+    public boolean isActive() {
+        return EUserStatus.ACTIVE.equals(status);
+    }
+}
