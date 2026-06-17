53856e4bcf364fa04f1f6b1a29c2bf1724379acc
Nathan <nathan.pignon79@gmail.com>
2026-06-17 10:49:28 +0200

    feat(1864): delete generic deletable repository

--- src/main/java/fr/avenirsesr/portfolio/common/data/domain/port/output/repository/GenericDeletableRepositoryPort.java
+++
@@ -1,8 +0,0 @@
-package fr.avenirsesr.portfolio.common.data.domain.port.output.repository;
-
-import fr.avenirsesr.portfolio.common.data.domain.model.DeletableAvenirsBaseModel;
-
-public interface GenericDeletableRepositoryPort<D extends DeletableAvenirsBaseModel>
-    extends GenericRepositoryPort<D> {
-  void delete(D domain);
-}
