33774c55786467f97fd5d8004b7b1b298306b90c
iamgreendev <56599296+BanelhaqB@users.noreply.github.com>
2026-07-30 19:23:38 +0200

    feat(external-user): add institutionId and groupId to ExternalUserDTO
    refs: #2348

--- src/main/java/fr/avenirsesr/portfolio/common/user/application/adapter/dto/ExternalUserDTO.java
+++ src/main/java/fr/avenirsesr/portfolio/common/user/application/adapter/dto/ExternalUserDTO.java
@@ -2,6 +2,7 @@ package fr.avenirsesr.portfolio.common.user.application.adapter.dto;
 
 import fr.avenirsesr.portfolio.common.data.domain.model.enums.EUserCategory;
 import fr.avenirsesr.portfolio.common.user.domain.model.enums.EUserStatus;
+import java.util.UUID;
 
 public record ExternalUserDTO(
     String eppn,
@@ -11,6 +12,8 @@ public record ExternalUserDTO(
     EUserCategory category,
     String externalId,
     String source,
+    UUID institutionId,
+    UUID groupId,
     EUserStatus status) {
 
   public boolean isActive() {
