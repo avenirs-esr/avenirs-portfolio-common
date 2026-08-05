be3ef791a7aef53fbe8ad0a09f54f24276b5047e
Nathan <nathan.pignon79@gmail.com>
2026-08-05 14:44:41 +0200

    fix: remove context id

--- src/main/java/fr/avenirsesr/portfolio/common/security/infrastructure/adapter/model/AvenirsSecurityHeaders.java
+++ src/main/java/fr/avenirsesr/portfolio/common/security/infrastructure/adapter/model/AvenirsSecurityHeaders.java
@@ -3,7 +3,6 @@ package fr.avenirsesr.portfolio.common.security.infrastructure.adapter.model;
 public class AvenirsSecurityHeaders {
   public static final String API_KEY = "X-API-Key";
   public static final String CONTEXT_SIGNATURE = "X-Context-Signature";
-  public static final String CONTEXT_KID = "X-Context-Kid";
   public static final String SIGNED_CONTEXT = "X-Signed-Context";
 
   private AvenirsSecurityHeaders() {}
